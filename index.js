10d0ded3ad2ecbdd58cfc4e3a63d89995552b593
Jeff Jewiss <jeff@jeffjewiss.com>
2018-01-24 16:51:54 +0000

    initial working version

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ import postcss from 'postcss'
 export default postcss.plugin('postcss-uncomment', (/*opts*/) => {
   return (root, /*result*/) => {
     root.walkDecls(decl => {
-      const originalValue = decl.value
+      if (decl.prop.match(/^\/\/.*/) && decl.raws.between.includes('#uncomment')) {
+        const cssString = decl.toString().replace('//', '').replace('#uncomment', '').trim()
+        const prop = cssString.split(':')[0].trim()
+        const value = cssString.split(':')[1].trim()
 
-      if (originalValue) {
-        // no-op
+        decl.replaceWith({ prop, value })
+      }
+    })
+
+    root.walkComments(comment => {
+      const cssString = comment.text.toString().replace('/*', '').replace('*/', '').replace('#uncomment', '').trim()
+      const prop = cssString.split(':')[0].trim()
+      const value = cssString.split(':')[1].trim()
+
+      if (comment.text.includes('#uncomment')) {
+        comment.replaceWith({ prop, value })
       }
     })
   }
